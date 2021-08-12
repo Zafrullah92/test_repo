767c8397f3b49c05f6a1b35db2c2dba9e072bb11
Mohammad Zafrullah Salim <zafrullah.salim@simedarbyplantation.com>
2021-08-12 12:52:11 +0800

    update

--- first script.R
+++ first script.R
@@ -7,8 +7,36 @@ head(formulation)
 
 require(MVN)
 
+#multivariate normality test
 mvn(formulation)
-
 mvn(data = formulation, mvnTest = 'royston', univariateTest = 'SW', desc = TRUE)
 
-view(formulation)
+#histogram for each ingredient
+hist(a, freq = FALSE)
+lines(density(a))
+
+hist(b, freq = F)
+lines(density(b))
+
+hist(c, freq = F)
+lines(density(c))
+
+hist(d, freq = F)
+lines(density(d))
+
+hist(e, freq = F)
+lines(density(e))
+
+hist(f, freq = F)
+lines(density(f))
+
+hist(g, freq = F)
+lines(density(g))
+
+hist(i, freq = F)
+lines(density(i))
+
+hist(h, freq = F)
+lines(density(h))
+
+#Cluster analysis
